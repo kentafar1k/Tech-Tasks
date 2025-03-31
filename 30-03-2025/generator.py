253c2969db85779dabad6245480d570ee8e6f4fa
kentafar1k <cherald1337@yandex.ru>
2025-03-31 07:40:44 +0300

    commit 31-03

--- 30-03-2025/generator.py
+++ 30-03-2025/generator.py
@@ -1,5 +1,5 @@
 def inner():
-    yield from (i * 2 for i in [1, 2, 3])
+    yield from (i * 2 for i in [1, 2, 3, 4])
     yield "Done"
 
 def outer():
@@ -8,3 +8,15 @@ def outer():
     yield "End"
 
 print(list(outer()))
+
+import asyncio
+
+async def s(a, b):
+    await asyncio.sleep(a)
+    print(b)
+
+async def main():
+    await s(1, 'hello')
+    await s(2, 'nigga')
+
+asyncio.run(main())
