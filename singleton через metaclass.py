53bfe4f54809bfa4e2d66c6aba92b2fe3a6794d6
kentafar1k <cherald1337@yandex.ru>
2025-04-06 10:45:40 +0300

    +singleton solve

--- singleton через metaclass.py
+++ singleton через metaclass.py
@@ -1,1 +1,14 @@
-...
+class SingletonMeta(type):
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):  # в обычном синглтоне метод __new__()
+        if cls not in cls._instances:
+            cls._instances[cls] = super().__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+class Singleton(metaclass=SingletonMeta):
+    pass
+
+a = Singleton()
+b = Singleton()
+print(id(a), id(b))
