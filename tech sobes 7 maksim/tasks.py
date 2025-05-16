56e6c30488753354c836cf1c72f5dd4dac64fb2c
kentafar1k <129420782+kentafar1k@users.noreply.github.com>
2025-05-16 12:51:41 +0300

    задача 1 с тех собеса

--- tech sobes 7 maksim/tasks.py
+++ tech sobes 7 maksim/tasks.py
@@ -8,4 +8,17 @@ def reverse_string(s):
 print(reverse_string(s))
 
 
-# 2 Написать декоратор, который перед
+# 2 Написать декоратор, который перед вызовом пишет Покупайте наших котиков # 3 добавить аргументы
+def params(arg):
+    def decorator(func):
+        def wrapper():
+            print(f"Покупайте наших {arg}!")
+            func()
+        return wrapper
+    return decorator
+
+@params('собачек')
+def my_func():
+    print('hi')
+
+my_func()
