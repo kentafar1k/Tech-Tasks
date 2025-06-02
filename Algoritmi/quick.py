a19392e6a27764aac2dcae60bb2412d227330bd7
kentafar1k <129420782+kentafar1k@users.noreply.github.com>
2025-06-02 17:40:42 +0300

    quick sort 1

---
+++ Algoritmi/quick.py
@@ -0,0 +1,15 @@
+arr = [5, 2, 4, 1]
+
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr  # База рекурсии
+
+    pivot = arr[0]  # Опорный элемент
+    left = [x for x in arr[1:] if x < pivot]       # Меньше pivot
+    right = [x for x in arr[1:] if x >= pivot]     # Больше или равны
+
+    return quick_sort(left) + [pivot] + quick_sort(right)
+
+
+print(arr)
+print(quick_sort(arr))
