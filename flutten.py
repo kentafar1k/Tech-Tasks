b8b72bc5c259293048c93ddd40b4fa843b62d0e2
kentafar1k <129420782+kentafar1k@users.noreply.github.com>
2025-06-12 16:20:59 +0300

    resolve flutten

---
+++ flutten.py
@@ -0,0 +1,12 @@
+l =[[1, 2, 3], [4, 5], 6]  # ->
+
+
+def flutten(lst) -> list:
+    for elem in lst:
+        if isinstance(elem, list):
+            yield from flutten(elem)
+        else:
+            yield elem
+
+
+print(list(flutten(l)))
