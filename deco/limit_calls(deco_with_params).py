2a0c9ca6e49b15dc03f00e18f6fb414a94a7305d
kentafar1k <129420782+kentafar1k@users.noreply.github.com>
2025-07-19 14:41:30 +0300

    написал декоратор с параметром лимит вызовов

---
+++ deco/limit_calls(deco_with_params).py
@@ -0,0 +1,27 @@
+from typing import Callable
+from functools import wraps
+import time
+
+
+def limit_calls(limit: int):
+    def wrapper(func: Callable) -> Callable:
+        @wraps(func)
+        def inner(*args, **kwargs):
+            nonlocal limit
+            if not limit:
+                return f"а всё"
+            res = func(*args, **kwargs)
+            limit -= 1
+            return res
+        return inner
+    return wrapper
+
+@limit_calls(3)
+def some_func():
+    return ('ky')
+
+print(some_func())
+print(some_func())
+print(some_func())
+print(some_func())
+print(some_func())
