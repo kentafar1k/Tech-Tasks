78a9407b7759a377bfbd06ba0c046c223be039a2
kentafar1k <129420782+kentafar1k@users.noreply.github.com>
2025-07-19 13:54:28 +0300

    дописал на дефолт дикт

--- parsing+defaultdict.py
+++ parsing+defaultdict.py
@@ -28,3 +28,49 @@ result = {
         }
     ]
 }
+
+from functools import defaultdict
+
+result = defaultdict(list)
+
+for server_name, server_data in d1.items():
+    owner = server_data['owner']
+    result[owner].append({
+        server_name: {
+            'cpu': server_data['cpu']
+        }
+    })
+
+# Преобразуем defaultdict обратно в обычный dict для вывода
+result = dict(result)
+print(result)
+
+def curtail_str(letters: str) -> str:
+    if not letters:
+        return ''
+    letters += '.'
+    result = ''
+    counter = 0
+    for i in range(len(letters) - 1):
+        if letters[i] != letters[i + 1]:
+            if not counter:
+                result += f'{letters[i]}'
+            else:
+                result += f'{letters[i]}{counter + 1}'
+            counter = 0
+        else:
+            counter += 1
+        print(result)
+
+    return result
+
+
+print("Hello Tochka")
+
+assert curtail_str('') == ''
+assert curtail_str("A") == "A"
+assert curtail_str('ABC') == 'ABC'
+assert curtail_str('AAA') == 'A3'
+assert curtail_str('AAABBXYZDDDDA') == 'A3B2XYZD4A'
+assert curtail_str('ABCABCABCCCCC') == 'ABCABCABC5'
+print('done')
